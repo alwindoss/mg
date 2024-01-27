f217f67cddf49e54fae6dff378ed5e78f3bdbf3f
Alwin Doss <alwindoss84@gmail.com>
2024-01-27 19:13:10 +0530

    fix error streaming to stderr
    Signed-off-by: Alwin Doss <alwindoss84@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,11 @@ use mg::{run, Config};
 fn main() {
     let args: Vec<String> = env::args().collect();
     let cfg = Config::build(&args).unwrap_or_else(|e| {
-        println!("problem parsing argument: {e}");
+        eprintln!("problem parsing argument: {e}");
         process::exit(-1);
     });
     if let Err(e) = run(cfg) {
-        println!("Application error: {e}");
+        eprintln!("Application error: {e}");
         process::exit(1);
     }
 }
